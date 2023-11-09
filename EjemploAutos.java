a58b6c0103809ee3b890bfb7bbbc524af2857729
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-11-08 19:34:19 -0500

    Update EjemploAutos.java

--- EjemploAutos.java
+++ EjemploAutos.java
@@ -10,11 +10,6 @@ public class EjemploAutos {
         nissan.cilindraje = 7.8;
 
         //Nissan
-//        System.out.println("Marca "+nissan.marca);
-//        System.out.println("Modelo "+nissan.modelo);
-//        System.out.println("Color "+nissan.color);
-//        System.out.println("Cilindraje "+nissan.cilindraje);
-
         System.out.println(nissan.detalleAuto());
         System.out.println(nissan.acelerar(3500));
         System.out.println(nissan.km(10000,5));
