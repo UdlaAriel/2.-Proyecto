1444a291a64eb2edc6b4608d1e31b1c648cde36d
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-11-08 19:35:38 -0500

    Update Autos.java

--- Autos.java
+++ Autos.java
@@ -4,27 +4,19 @@ public class Autos {
     String color = "Rosadooo";
     Double cilindraje;
     public String detalleAuto(){
-
-        //        String color = "Verde";
-//        System.out.println("Marca "+ this.marca);
-//        System.out.println("Modelo "+this.modelo);
-//        System.out.println("Color "+this.color);
-//        System.out.println("Cilindraje "+this.cilindraje);
-
-        String da = "\nMarca " + this.marca +
+        String detalle = "\nMarca " + this.marca +
                 "\nModelo " + this.modelo +
                 "\nColor " + this.color +
                 "\nCilindraje " + this.cilindraje;
 
-        return da;
+        return detalle;
     }
 
     public String acelerar(int rpm){
         return "Auto " + this.marca + " el auto esta acelerando a "+ rpm + " rpm";
-
     }
 
     public String km (int kilometraje, int years){
-        return "El auto " + this.marca + "posee un kilometraje de " + kilometraje + " km. Tiene " + years + " anios.";
+        return "El auto " + this.marca + " posee un kilometraje de " + kilometraje + " km. Tiene " + years + " años.";
     }
 }
